280f21c4f36039cd4403e0c2d8d2abd6640b7b5e
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-08-19 13:38:05 +0700

    fix: deploy-

--- apps/web/src/utils/config.ts
+++ apps/web/src/utils/config.ts
@@ -1,4 +1,4 @@
 export const appConfig = {
-  baseUrl: 'https://tes-gefima-web.vercel.app/api',
-  baseUrlNext: 'https://tes-gefima-web.vercel.app',
+  baseUrl: 'http://localhost:8000/api',
+  baseUrlNext: 'http://localhost:3000',
 };
