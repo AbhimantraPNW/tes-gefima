d9cd84e7e4f846f2f18a2b985c969bc3f981d583
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-08-20 08:15:08 +0700

    fix: deploy--

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -14,4 +14,3 @@ export const PORT = process.env.PORT || 8000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
 export const JWT_SECRET = process.env.JWT_SECRET || '';
 export const NEXT_BASE_URL = process.env.NEXT_BASE_URL || '';
-
